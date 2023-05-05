1bd46a0fc9df9b3b16ade868ca6b05f38e3efdc8
Snadderbyte <69865773+Snadderbyte@users.noreply.github.com>
2023-05-05 18:38:26 +0200

    Add project files.

---
+++ DAT154-Oblig4-/Shared/TaskValidation.cs
@@ -0,0 +1,32 @@
+﻿using DAT154_Oblig4_.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAT154_Oblig4_.Shared;
+
+public partial class TaskValidation
+{
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime? Date { get; set; }
+    [Required]
+    public string? Status { get; set; }
+    [Required]
+    public string? Type { get; set; }
+
+    public string? Note { get; set; }
+    [Required]
+    public int? RoomId { get; set; } 
+
+    public int? StaffId { get; set; }
+
+    public Room? Room = null;
+    public User? User = null;
+}
---
+++ DAT154-Oblig4-/Shared/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace DAT154_Oblig4_.Shared
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
+}
